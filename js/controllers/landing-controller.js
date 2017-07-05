1307b3f471bfea6b9c4e9027517c22e0e65fed3e
felipemaciel <felipemrvieira@gmail.com>
2017-07-04 22:33:43 -0300

    adicionado belzevier e smooth scroll

--- js/controllers/landing-controller.js
+++ js/controllers/landing-controller.js
@@ -92,7 +92,7 @@ function($scope, $sce, $location){
   var options = {
     strings: ["Somos marketing", "Somos websites", "Somos aplicativos", "Somos mídias sociais", "Somos StarFish"],
     typeSpeed: 80,
-    backSpeed: 100
+    backSpeed: 50
   }
   var typed = new Typed("#typed", options);
 
@@ -109,5 +109,87 @@ function($scope, $sce, $location){
 		.addIndicators({name: "trigger pin"})
 		.addTo(controller);
 	});
+
+  $(function () { // wait for document ready
+		var flightpath = {
+			entry : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 100,	y: -20},
+						{x: 300,	y: 10}
+					]
+			},
+			looping : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 510,	y: 40},
+						{x: 620,	y: -60},
+						{x: 500,	y: -100},
+						{x: 380,	y: 20},
+						{x: 500,	y: 40},
+						{x: 580,	y: 20},
+						{x: 620,	y: 15}
+					]
+			},
+			leave : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 660,	y: -100},
+						{x: 800,	y: -300},
+						{x: $(window).width() + 300,	y: -100},
+					]
+			}
+		};
+		// init controller
+		var controller2 = new ScrollMagic.Controller();
+
+		// create tween
+		var tween = new TimelineMax()
+			.add(TweenMax.to($("#ovni"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#ovni"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#ovni"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+
+		// build scene
+		var scene = new ScrollMagic.Scene({
+      triggerElement: "#sec-2",
+      duration: 600,
+      triggerHook: 0.1
+    })
+						.setPin("#ovni")
+						.setTween(tween)
+            .addIndicators({name: "trigger rotate"})
+						.addTo(controller2);
+	});
+
+  $(function(){
+
+	var $window = $(window);		//Window object
+
+	var scrollTime = 1;			//Scroll time
+	var scrollDistance = 80;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+	$window.on("mousewheel DOMMouseScroll", function(event){
+
+		event.preventDefault();
+
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5
+			});
+
+	});
+
+});
+
+
 
 }]);
